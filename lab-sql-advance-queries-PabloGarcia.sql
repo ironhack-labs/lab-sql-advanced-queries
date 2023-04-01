c4246dd9fbb0f2c7ae16206158cc2abef2d538c9
Blitu82 <garpaues@yahoo.es>
2023-04-01 15:52:57 +0100

    [lab-sql-advanced-queries]PabloGarcia

--- lab-sql-advance-queries-PabloGarcia.sql
+++ lab-sql-advance-queries-PabloGarcia.sql
@@ -11,23 +11,22 @@ SELECT * FROM pair_of_actors;
 
 -- 2. For each film, list actor that appeared in that film that has appeared in the most films overall. film_id, actor_id that appeared
 -- Step 1: Temp table with film count per actor
-
-
--- Step2: CTE with rank
--- WITH film_count_rank AS (SELECT actor_id, film_count, rank() OVER (ORDER BY film_count DESC) as rnk FROM film_count)
--- SELECT * FROM film_count_rank;
-
--- Step 3: Get all cols filter by rank. For each film, list actor that appeared in that film that has appeared in the most films overall
 CREATE TEMPORARY TABLE film_count
 SELECT actor_id, COUNT(film_id) as film_count 
 FROM sakila.film_actor
 GROUP BY actor_id;
-WITH film_count_rank AS (
-  SELECT actor_id, film_count, RANK() OVER (ORDER BY film_count DESC) AS rnk
-  FROM film_count
-)
+
+-- Step2: CTE with rank
+WITH film_count_rank AS (SELECT actor_id, film_count, rank() OVER (ORDER BY film_count DESC) as rnk FROM film_count)
+SELECT * FROM film_count_rank;
+
+-- Step 3: Get all cols filter by rank. For each film, list actor that appeared in that film that has appeared in the most films overall
+
+
 SELECT B.title, C.first_name, C.last_name FROM sakila.film_actor A
 JOIN sakila.film B USING(film_id)
 JOIN sakila.actor C USING(actor_id)
 WHERE actor_id = (SELECT actor_id FROM film_count_rank WHERE rnk = (SELECT MAX(rnk) FROM film_count_rank))
 ORDER BY B.title;
+
+
